94c56db9d2fdb25c60da93e579157d84a51dfb05
Rijish13Ahuja <rhythm00ahuja@gmail.com>
2024-05-30 23:34:38 +0530

    o

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 chrome.runtime.sendMessage('getImageUrl', imageUrl => {
-\    const profilePictures = document.querySelectorAll('img.update-components-actor__avatar-image');
+  const profilePictures = document.querySelectorAll('img.update-components-actor__avatar-image');
     profilePictures.forEach(img => {
         img.src = imageUrl;
         img.srcset = imageUrl; 
